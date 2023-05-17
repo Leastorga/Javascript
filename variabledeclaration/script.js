83e9cace77adc67767c21e93a35e6980b2dab0ef
Letícia Astorga <leticia.trancozo@gmail.com>
2023-05-16 22:34:53 -0300

    Update script.js

--- variabledeclaration/script.js
+++ variabledeclaration/script.js
@@ -1,1 +1,29 @@
+function soma(){
+    let numero1 = parseInt(prompt("Digite um número:"))
+    let numero2 = parseInt(prompt("Digite outro número:"))
+    let soma = numero1 + numero2
+    alert(soma)
+}
+
+function subtracao(){
+    let numero1 = parseInt(prompt("Digite um número:"))
+    let numero2 = parseInt(prompt("Digite outro número:"))
+    let subtracao = numero1 - numero2
+    alert(subtracao)
+}
+
+function multiplicacao(){
+    let numero1 = parseInt(prompt("Digite um número:"))
+    let numero2 = parseInt(prompt("Digite outro número:"))
+    let multiplicacao = numero1 * numero2
+    alert(multiplicacao)
+}
+
+function divisao(){
+    let numero1 = parseFloat(prompt("Digite um número:"))
+    let numero2 = parseFloat(prompt("Digite outro número"))
+    let divisao = numero1 / numero2
+    alert(divisao).toFixed(2)
+}
+
 
